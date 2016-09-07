e4858e11880554fba4b02ad0fe6fbf7a7f66b725
sangwon <sangwon.s@ateamventures.com>
2016-09-07 12:14:47 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# random-code-generator
+
+## How to use
+```
+Usage: python random_code_generator.py --count {count} --length {length} [--hyphen]
+```
+
+```shell
+$ python random_code_generator.py --count 10 --length 16
+lgppuexnPKPhMPbB
+2GOT21VzAPWAAjwX
+q8ThQyVJ5Y4pPHOK
+9hHQIYcMgVuDguAe
+VfzWjROjmoaHU0Je
+Xsq8UPrA4Nv6MWM4
+XtIfwN4UCsa9WkAF
+daqx5W8sK7dJAlWK
+FBAQpOpKie1rqgsq
+wPfN71gYgDvVvvTb
+```
+
+```shell
+$ python random_code_generator.py --count 10 --length 16 --hyphen
+fuNc-0X2d-9n0Q-11SR
+ufw6-JjFN-bbJ4-ymFK
+iT94-Gv3R-zjp5-W9FW
+ATj7-1r5t-dwmt-Pn2w
+fBwD-nU6W-QZ9O-2rKl
+n7wI-l1i4-QPlE-22PD
+Q38n-UvkY-TT1O-b24E
+1AwA-lmPj-P6sK-lmqY
+CoYM-OMsb-pV8w-veGc
+NJ9N-c4Pz-3d30-rddu
+```
---
+++ random_code_generator.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import random
+import string
+from optparse import OptionParser
+
+
+def generate_random_code(count, length, show_hyphen=False):
+    alnum = string.ascii_letters + string.digits
+    for i in range(count):
+        serial = ''
+        for x in range(length):
+            serial += random.choice(alnum)
+            if show_hyphen and (x + 1) % 4 == 0 and (x + 1) != length:
+                serial += '-'
+        print(serial)
+
+
+def usage():
+    print('Usage: python random_code_generator.py '
+          '--count {count} --length {length} [--hyphen]')
+
+
+if __name__ == '__main__':
+    import sys
+    parser = OptionParser()
+    parser.add_option('--count', dest='count', type='int')
+    parser.add_option('--length', dest='length', type='int')
+    parser.add_option('--hyphen', action='store_true',
+                      dest='show_hyphen', default=False)
+    (options, args) = parser.parse_args()
+
+    count = options.count
+    length = options.length
+    show_hyphen = options.show_hyphen
+
+    try:
+        assert count
+        assert length
+
+        generate_random_code(count, length, show_hyphen)
+    except AssertionError:
+        usage()
+        sys.exit(1)
+
+# vim: set fenc=utf8 expandtab sw=4 ts=4:
